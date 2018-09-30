9d549b5cd874db065e667becdf774f053cbec4ce
Dan <danillogomes76@yahoo.com.br>
2018-09-30 14:49:11 -0300

    Testa alteração de lider

--- Codigos/PHP/testaprimeiravez.php
+++ Codigos/PHP/testaprimeiravez.php
@@ -1,5 +1,18 @@
 <?php  
 	include("conexao.php"); 
+
+	date_default_timezone_set('America/Sao_Paulo');
+	$date = date('Y-m-d');
+
+
+	$con = "SELECT * FROM mudancas_lider WHERE  data_saida = '$date'"; 
+    $result = $mysqli->query($con) or die($mysqli->error); 
+
+    while($dados = $result->fetch_array())
+    {
+    	$mudanca = "UPDATE grupo_pesquisa SET id_lider = '$dados["id_novo_lider"]' WHERE  id_lider = '$dados["id_lider_antigo"]'";
+    }
+
  
 	$consulta = "SELECT * FROM usuarios "; 
     $result = $mysqli->query($consulta) or die($mysqli->error); 
