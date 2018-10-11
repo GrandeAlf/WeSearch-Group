a0467e8b53f42e8af07979af46600805b633ca66
DAN <danillogomes76@yahoo.com.br>
2018-10-10 21:48:11 -0300

    Telas Incremento III

---
+++ Codigos/PHP/areas_pesquisa.php
@@ -0,0 +1,99 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    
+
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+
+
+
+<link rel="stylesheet" type="text/css" href="../CSS/perm.css">
+    <title></title>
+
+</head>
+<body>
+
+
+        
+    <table class="table table-bordered table-sm m-0">
+                    <thead class="">
+                        <tr>
+                            <th>#</th>
+                            <th>Nome</th>
+                            
+                            
+                                
+                                <th align="center"></th>
+                                
+                            
+                          
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                      
+                             <tr>
+                                <td align="center"><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                 
+                                <td align="center">EXEMPLO DE LINHA</td>
+                                
+                               
+                                <td align="center"><a href=""><button class="btn btn-warning btn-block">Alterar Linha</button>
+                                
+                                </td>
+
+                                </tr> 
+
+                                 <tr>
+                                <td align="center"><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                 
+                                <td align="center">EXEMPLO DE LINHA</td>
+                                
+                               
+                                <td align="center"><a href=""><button class="btn btn-warning btn-block">Alterar Linha</button>
+                                
+                                </td>
+
+                                </tr> 
+
+
+                                 <tr>
+                                <td align="center"><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                 
+                                <td align="center">EXEMPLO DE LINHA</td>
+                                
+                               
+                                <td align="center"><a href=""><button class="btn btn-warning btn-block">Alterar Linha</button>
+                                
+                                </td>
+
+                                </tr> 
+                      
+                    </tbody>
+
+                </table>
+
+                <br>
+               
+                      <a href=""><button class="btn btn-success">Cadastrar Linha</button></a>
+
+                      <a href=""><button class="btn btn-danger ">Excluir Linhas</button>
+
+
+               
+            
+    
+</body>
+</html>
+
+            
---
+++ Codigos/PHP/docente.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    
+     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+    
+     
+       <link rel="stylesheet" type="text/css" href="../CSS/login.css">
+
+    <title>Docente</title>
+  </head>
+  <body>
+    
+    <div class="wrapper" >
+      <form class="form-signin" method="post" action="validalogin.php"> 
+         <div class="text-center">
+          <h2 class="form-signin-heading">Docente</h2>
+
+          <!-- Apresentar foto -->
+          <h3><i class="fa fa-user fa-4x"></i></h3> 
+
+
+         </div>   
+<br><br><br>        
+<div class="text-center">
+          <p>Nome: Michael Jackson</p> <br>
+          <p><a href="">link lattes</a></p> <br>
+          <p>Ativiade realizada no grupo: Moonwalker</p> <br>
+          <p>Graduação: Superior Completo</p> <br>
+          <p>Linhas de Pesquisa: Dancing Bitch</p> <br>
+          <p>Data de vinculação linha de pesquisa: 10/02/2014</p> <br>
+          <p>Data da inclusão no grupo: 21/08/2018</p> <br>
+
+</div>
+          
+
+          </div>
+      </form>
+    </div>
+
+    
+    
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  </body>
+</html>
