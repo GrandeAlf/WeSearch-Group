1c974c9eb31ca72787e7f7013a3b0a896f9eabd9
DAN <danillogomes76@yahoo.com.br>
2018-10-29 22:13:32 -0300

    2 telas incremento 4

---
+++ Codigos/PHP/cadastrar_projetodepesquisa.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+   
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+    <script src="http://momentjs.com/downloads/moment-with-locales.js"></script>
+    <script src="http://momentjs.com/downloads/moment-timezone-with-data.js"></script>
+    
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+     <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/css/bootstrap-select.min.css" />
+
+     <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/css/bootstrap-select.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    
+
+    <!-- (Optional) Latest compiled and minified JavaScript translation files -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/js/i18n/defaults-*.min.js"></script>
+    
+    
+     
+       <link rel="stylesheet" type="text/css" href="../CSS/login.css">
+       <script type="text/javascript" src="../JS/scripts.js"></script>
+  </head>
+   
+
+<?php include("funcoes.php");
+     
+    
+?>
+    <title>Cadastrar Projeto</title>
+  </head>
+  
+<body onload="barra()">
+	
+   <div id="barra">
+     
+
+   </div>
+
+  <div >
+        <form class="form-signin"  method="post" action="validaativacaogrupos.php">       
+      <h2 class="form-signin-heading" align="center">Projeto de Pesquisa</h2>
+      <input  type="text" class="form-control" name="nome" placeholder="Título do projeto de pesquisa"   />
+      <select  name="linha" class="selectpicker form-control">
+         <option  selected="selected" >Docente Responsavel</option>
+         
+
+      </select>
+      <br><br>
+      <select data-live-search="true" name="linha" class="selectpicker form-control">
+         <option disabled selected="selected" >Linha de Pesquisa</option>
+         
+
+      </select>
+      <br><br>
+      <select  name="linha" class="selectpicker form-control">
+         <option  selected="selected" >Tipo</option>
+         
+
+      </select>
+      <br><br>
+      <input disabled type="text" class="form-control" name="outro" placeholder="Tipo da bolsa"   />
+      <select  name="linha" class="selectpicker form-control">
+         <option  selected="selected" >Orientado</option>
+         
+
+      </select>
+      <br><br>
+       <p>Data de Inclusão da Linha de Pesquisa</p>
+      <div class="input-group registration-date-time">
+                    <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span></span>
+                    <input class="form-control" name="inclusao" id="registration-date" type="date">
+                    
+                </div>
+                
+
+      <br>
+       <p>Data de Remoção da Linha de Pesquisa</p>
+      <div class="input-group registration-date-time">
+                    <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span></span>
+                    <input class="form-control" name="inclusao" id="registration-date" type="date">
+                    
+                </div>
+                
+
+      <br>
+        
+        <input class="btn btn-lg btn-block btn-success" type="submit" name="cadastrar" value="Alterar"/><br>
+        
+    </form>
+      </div>
+    </div>
+    
+  </div>
+
+	 
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>
+    
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/js/i18n/defaults-*.min.js"></script>
+</body>
+</html>
