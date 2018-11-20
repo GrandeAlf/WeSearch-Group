c037371d546bf4dfc831b92f105e14940df92400
Dan <danillogomes76@yahoo.com.br>
2018-11-20 18:45:14 -0200

    .

---
+++ Codigos/PHP/seleciona_relatorios.php
@@ -0,0 +1,99 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<?php include("conexao.php");
+       include("funcoes.php");
+     
+    // $logado = logado();
+     //if($logado == NULL)
+     //{
+     // header("location: inicial.php");
+     //}
+    
+
+
+
+       $lider = lider();
+       $adm = dado(); 
+        
+             
+      
+  ?>
+
+
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+<link rel="stylesheet" type="text/css" href="../CSS/perm.css">
+    <title></title>
+
+</head>
+<body>
+
+
+    <div id = "conteudo">    
+
+      <table class="table table-bordered table-sm m-0">
+                    <thead class="">
+                        <tr>
+                                      
+                        </tr>
+                    </thead>
+                    <tbody>
+                       
+                             <tr>
+                                 
+                                <td align="center"><a href=""><button class="btn btn-primary btn-block">Finalizar Cadastro</button>
+                                
+                                </td>
+                                
+
+                              </tr> 
+
+                                <tr>
+                                 
+                                <td align="center"><a href=""><button class="btn btn-success btn-block">Finalizar Cadastro</button>
+                                
+                                </td>
+                                </tr>
+
+                                <tr>
+                                 
+                                <td align="center"><a href=""><button class="btn btn-danger btn-block">Finalizar Cadastro</button>
+                                
+                                </td>
+                                </tr>
+                                 
+                                 <tr>
+
+                                <td align="center"><a href=""><button class="btn btn-warning btn-block">Finalizar Cadastro</button>
+                                
+                                </td>
+                                </tr>
+                                
+                                <tr> 
+                                <td align="center"><a href=""><button class="btn btn-info btn-block">Finalizar Cadastro</button>
+                                
+                                </td>
+                                </tr>
+                                                                 
+                                
+                      
+
+                    </tbody>
+
+                </table>
+               
+               
+            
+    
+
+</div>   
+    
+</body>
+</html>
+
+            
